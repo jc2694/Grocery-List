36065303939943d019646800be1d415f32ceda0a
jc2694 <jcrobertson@shaw.ca>
2017-07-21 23:29:38 -0400

    Add files via upload

--- Grocery List SImple/index.php
+++ Grocery List SImple/index.php
@@ -1,35 +1,10 @@
 <?php
 
+require_once('Request.php');
 
-// get the HTTP method, path and body of the request
-$method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$request = new Request();
+$request->parseIncomingParams();
 
-var_dump($method, $request);
-exit(0);
-
-$input = json_decode(file_get_contents('php://input'),true);
- 
-// connect to the mysql database
-$link = mysqli_connect('localhost', 'user', 'pass', 'dbname');
-mysqli_set_charset($link,'utf8');
- 
-// retrieve the table and key from the path
-$table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-$key = array_shift($request)+0;
- 
-// escape the columns and values from the input object
-$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
-$values = array_map(function ($value) use ($link) {
-  if ($value===null) return null;
-  return mysqli_real_escape_string($link,(string)$value);
-},array_values($input));
- 
-// build the SET part of the SQL command
-$set = '';
-for ($i=0;$i<count($columns);$i++) {
-  $set.=($i>0?',':'').'`'.$columns[$i].'`=';
-  $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
-}
+var_dump($request->parameters);
 
 ?>
